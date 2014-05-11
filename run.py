86031724f13e709dc3db8c28efafe4c74833a785
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2014-05-11 10:52:38 +0200

    Add missing parameter when creating app (MATRIKKEL_BASE_URL).

--- run.py
+++ run.py
@@ -8,7 +8,12 @@ from app import create_app
 username = os.environ["FLOD_MATRIKKEL_USER"]
 password = os.environ["FLOD_MATRIKKEL_PASS"]
 
+matrikkel_base_url = os.environ["MATRIKKEL_BASE_URL"]
 matrikkel_user = os.environ["MATRIKKEL_USERNAME"]
 matrikkel_password = os.environ["MATRIKKEL_PASSWORD"]
 
-application = create_app(username, password, matrikkel_user, matrikkel_password)
+application = create_app(username,
+                         password,
+                         matrikkel_base_url,
+                         matrikkel_user,
+                         matrikkel_password)
